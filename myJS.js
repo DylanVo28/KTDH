b18cc15782cf21325f214d054f36a7309900ee30
DinhVo <dinh.vo@mediastep.com>
2021-03-30 23:30:22 +0700

    test draw line

--- myJS.js
+++ myJS.js
@@ -7,12 +7,38 @@ let w = 1280
 let h = 600
 
 initCoordinate(w,h)
-canvas.onmousedown =  function (e) {
-   
-    const posX = getPosX(e);
-    const posY = getPosY(e)
-    ctx.fillStyle = "red";
-    
-    ctx.fillRect(posX, posY,5,5)
+// canvas.onmousedown =  function (e) {
+//     const posX = getPosX(e);
+//     const posY = getPosY(e)
+//     ctx.fillStyle = "red";
+//     ctx.fillRect(posX, posY,5,5)
+// }
+
+var checkClickFirst=false;
+var pointPrev={
+    x:0,
+    y:0
+}
+canvas.onmousedown=function(e){
+    const posX=getPosX(e)
+    const posY=getPosY(e)
+    if(!checkClickFirst){
+        pointPrev={
+            x:posX,
+            y:posY
+        }
+     checkClickFirst=true;
+    }
+    else{
+        
+        const point={
+            x:posX,
+            y:posY
+        }
+        console.log(pointPrev.x+" "+pointPrev.y+" "+point.x+" "+point.y)
+        drawLine(pointPrev.x,pointPrev.y,point.x,point.y)
+        pointPrev=point
+    }
     
 }
+
