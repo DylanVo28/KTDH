bd72e84c980b49fd78fe940fa48c443f3bad64d3
DinhVo <dinh.vo@mediastep.com>
2021-04-03 16:15:49 +0700

    code features DrawLine,ClickElement

--- myJS.js
+++ myJS.js
@@ -5,39 +5,55 @@ var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
 let w = 1280
 let h = 600
-
 initCoordinate(w,h)
-// canvas.onmousedown =  function (e) {
-//     const posX = getPosX(e);
-//     const posY = getPosY(e)
-//     ctx.fillStyle = "red";
-//     ctx.fillRect(posX, posY,5,5)
-// }
+var buttons=document.getElementsByClassName('btn');
+for (var i=0; i < buttons.length; i++) {
+    buttons[i].onclick = function(){
+        optionSelect(event.srcElement.value)
+    }
+};
 
-var checkClickFirst=false;
-var pointPrev={
-    x:0,
-    y:0
-}
-canvas.onmousedown=function(e){
-    const posX=getPosX(e)
-    const posY=getPosY(e)
-    if(!checkClickFirst){
-        pointPrev={
-            x:posX,
-            y:posY
+function optionSelect(action){
+    switch(action){
+            case 'CLICK_ELEMENT':
+                canvas.onmousedown =  function (e) {
+                    const posX = getPosX(e);
+                    const posY = getPosY(e)
+                    ctx.fillStyle = "red";
+                    ctx.fillRect(posX, posY,5,5)
+                }
+                break;
+            case 'DRAW_LINE':
+                var checkClickFirst=false;
+                var pointPrev={
+                    x:0,
+                    y:0
+                }
+                canvas.onmousedown=function(e){
+                    const posX=getPosX(e)
+                    const posY=getPosY(e)
+                    ctx.fillStyle = "red";
+                    ctx.fillRect(posX, posY,6,6)
+                    if(!checkClickFirst){
+                        pointPrev={
+                            x:posX,
+                            y:posY
+                        }
+                    checkClickFirst=true;
+                    }
+                    else{
+                        
+                        const point={
+                            x:posX,
+                            y:posY
+                        }
+                        drawLine(pointPrev.x,pointPrev.y,point.x,point.y)
+                        pointPrev=point
+                    }
+                    
+                }
+                break;  
+            
         }
-     checkClickFirst=true;
-    }
-    else{
-        
-        const point={
-            x:posX,
-            y:posY
-        }
-        console.log(pointPrev.x+" "+pointPrev.y+" "+point.x+" "+point.y)
-        drawLine(pointPrev.x,pointPrev.y,point.x,point.y)
-        pointPrev=point
-    }
-    
 }
+
