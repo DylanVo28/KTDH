b40c4f62ba1ca250868588943eb925fc102e8e24
DinhVo <dinh.vo@mediastep.com>
2021-04-04 15:04:11 +0700

    dasdsa

--- myJS.js
+++ myJS.js
@@ -5,8 +5,8 @@ var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
 let w = 1280
 let h = 600
-initCoordinate(w,h)
 var buttons=document.getElementsByClassName('btn');
+initCoordinate(w,h)
 for (var i=0; i < buttons.length; i++) {
     buttons[i].onclick = function(){
         optionSelect(event.srcElement.value)
@@ -15,45 +15,47 @@ for (var i=0; i < buttons.length; i++) {
 
 function optionSelect(action){
     switch(action){
-            case 'CLICK_ELEMENT':
-                canvas.onmousedown =  function (e) {
-                    const posX = getPosX(e);
-                    const posY = getPosY(e)
-                    ctx.fillStyle = "red";
-                    ctx.fillRect(posX, posY,5,5)
-                }
-                break;
-            case 'DRAW_LINE':
-                var checkClickFirst=false;
-                var pointPrev={
-                    x:0,
-                    y:0
-                }
-                canvas.onmousedown=function(e){
-                    const posX=getPosX(e)
-                    const posY=getPosY(e)
-                    ctx.fillStyle = "red";
-                    ctx.fillRect(posX, posY,6,6)
-                    if(!checkClickFirst){
-                        pointPrev={
-                            x:posX,
-                            y:posY
-                        }
-                    checkClickFirst=true;
+        case 'CLICK_ELEMENT':
+            canvas.onmousedown =  function (e) {
+                const posX = getPosX(e);
+                const posY = getPosY(e)
+                ctx.fillStyle = "red";
+                ctx.fillRect(posX, posY,5,5)
+            }
+            break;
+        case 'DRAW_LINE':
+            var checkClickFirst=false;
+            var pointPrev={
+                x:0,
+                y:0
+            }
+            canvas.onmousedown=function(e){
+                const posX=getPosX(e)
+                const posY=getPosY(e)
+                ctx.fillStyle = "red";
+                ctx.fillRect(posX, posY,6,6)
+                if(!checkClickFirst){
+                    pointPrev={
+                        x:posX,
+                        y:posY
                     }
-                    else{
-                        
-                        const point={
-                            x:posX,
-                            y:posY
-                        }
-                        drawLine(pointPrev.x,pointPrev.y,point.x,point.y)
-                        pointPrev=point
-                    }
+                checkClickFirst=true;
+                }
+                else{
                     
+                    const point={
+                        x:posX,
+                        y:posY
+                    }
+                    drawLine(pointPrev.x,pointPrev.y,point.x,point.y)
+                    pointPrev=point
                 }
-                break;  
-            
+                
+            }
+            break;
+        default :
+            alert("Vui lòng chọn tính năng")      
         }
+        
 }
 
